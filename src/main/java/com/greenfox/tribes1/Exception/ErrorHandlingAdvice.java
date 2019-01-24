7552fc0de377783c73fcbf04c0d99dc93cf7302c
Enikő Tóthová <toeraab.avf@gmail.com>
2019-01-24 13:20:29 +0100

    missing parameter exception resolved

--- src/main/java/com/greenfox/tribes1/Exception/ErrorHandlingAdvice.java
+++ src/main/java/com/greenfox/tribes1/Exception/ErrorHandlingAdvice.java
@@ -1,11 +1,15 @@
 package com.greenfox.tribes1.Exception;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @ControllerAdvice
 public class ErrorHandlingAdvice {
@@ -28,7 +32,11 @@ public class ErrorHandlingAdvice {
   @ExceptionHandler(MethodArgumentNotValidException.class)
   @ResponseStatus(HttpStatus.BAD_REQUEST)
   ErrorMsg missingParams(MethodArgumentNotValidException ex){
-    return new ErrorMsg("error","Missing parameters: username or password missing");
+    Map<String, String> errors = new HashMap<>();
+    for (FieldError fieldError : ex.getBindingResult().getFieldErrors()) {
+      errors.put(fieldError.getField(), fieldError.getDefaultMessage());
+    }
+    return new ErrorMsg("error","Missing parameter(s): " + errors);
   }
 
   @ResponseBody
