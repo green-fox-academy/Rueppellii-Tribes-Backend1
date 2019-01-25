68edd0cc5f62dd0cb39008db172991161f3614b2
lyancsie <info@fordit.biz>
2019-01-25 16:18:55 +0100

    SQL simplified, renamed, refactored

--- src/main/resources/db/migration/mySQL/V1_1__init.sql
+++ src/main/resources/db/migration/mySQL/V1_1__init.sql
@@ -1,6 +1,6 @@
 CREATE TABLE `application_user` (
   `id`         bigint(20) NOT NULL AUTO_INCREMENT,
-  `password`   varchar(255)        DEFAULT NULL,
+  `password`   varchar(255),
   `user_email` varchar(255)        DEFAULT NULL,
   `username`   varchar(255)        DEFAULT NULL,
   PRIMARY KEY (`id`)
