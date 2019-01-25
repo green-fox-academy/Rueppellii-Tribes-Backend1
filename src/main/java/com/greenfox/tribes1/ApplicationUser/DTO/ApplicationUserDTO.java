fa89d70c0f431cfbd780815083c10ef693e6e932
bleaksmile <38665050+bleaksmile@users.noreply.github.com>
2019-01-25 10:09:33 +0100

    Update ApplicationUserDTO.java

--- src/main/java/com/greenfox/tribes1/ApplicationUser/DTO/ApplicationUserDTO.java
+++ src/main/java/com/greenfox/tribes1/ApplicationUser/DTO/ApplicationUserDTO.java
@@ -1,6 +1,8 @@
 package com.greenfox.tribes1.ApplicationUser.DTO;
 
-import lombok.*;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.NoArgsConstructor;
 
 import javax.validation.constraints.NotBlank;
 
