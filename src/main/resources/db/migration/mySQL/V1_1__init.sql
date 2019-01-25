f7ffbaff9979d5209b6ebd384cc8bc3fa3ac3a9b
lyancsie <info@fordit.biz>
2019-01-25 16:06:11 +0100

    unnecessary lines removed

--- src/main/resources/db/migration/mySQL/V1_1__init.sql
+++ src/main/resources/db/migration/mySQL/V1_1__init.sql
@@ -4,19 +4,13 @@ CREATE TABLE `application_user` (
   `user_email` varchar(255)        DEFAULT NULL,
   `username`   varchar(255)        DEFAULT NULL,
   PRIMARY KEY (`id`)
-)
-  ENGINE = InnoDB
-  DEFAULT CHARSET = utf8mb4
-  COLLATE = utf8mb4_0900_ai_ci;
+);
 
 CREATE TABLE `kingdom` (
   `id`   bigint(20) NOT NULL AUTO_INCREMENT,
   `name` varchar(255)        DEFAULT NULL,
   PRIMARY KEY (`id`)
-)
-  ENGINE = InnoDB
-  DEFAULT CHARSET = utf8mb4
-  COLLATE = utf8mb4_0900_ai_ci;
+);
 
 CREATE TABLE `application_user_kingdom` (
   `kingdom_id`          bigint(20) DEFAULT NULL,
@@ -25,7 +19,4 @@ CREATE TABLE `application_user_kingdom` (
   KEY `FKlri4ciyecg9opp9r3ow0b4lp1` (`kingdom_id`),
   CONSTRAINT `FKj725o1t4n56nlb9fysdcw8r3b` FOREIGN KEY (`application_user_id`) REFERENCES `application_user` (`id`),
   CONSTRAINT `FKlri4ciyecg9opp9r3ow0b4lp1` FOREIGN KEY (`kingdom_id`) REFERENCES `kingdom` (`id`)
-)
-  ENGINE = InnoDB
-  DEFAULT CHARSET = utf8mb4
-  COLLATE = utf8mb4_0900_ai_ci;
+);
