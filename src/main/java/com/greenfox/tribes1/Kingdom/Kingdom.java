51ef5210f8b3a816eb773942620de301033d7c9d
ChrisFrill <fkrisztian93@gmail.com>
2019-01-28 15:12:47 +0100

    Added connection between Troop and Kigndom

--- src/main/java/com/greenfox/tribes1/Kingdom/Kingdom.java
+++ src/main/java/com/greenfox/tribes1/Kingdom/Kingdom.java
@@ -3,6 +3,7 @@ package com.greenfox.tribes1.Kingdom;
 import com.fasterxml.jackson.annotation.JsonBackReference;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.greenfox.tribes1.ApplicationUser.ApplicationUser;
+import com.greenfox.tribes1.Troop.Troop;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -29,6 +30,9 @@ public class Kingdom {
 
   @OneToOne(mappedBy = "kingdom")
   ApplicationUser applicationUser;
+
+  @OneToOne(mappedBy = "kingdom")
+  private Troop troop;
 
   public Kingdom(String name) {
     this.name = name;
