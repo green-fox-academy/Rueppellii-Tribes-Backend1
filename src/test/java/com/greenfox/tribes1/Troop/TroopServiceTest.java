bd30d19ff7eadb183c8047881e0308646d3d1491
ChrisFrill <fkrisztian93@gmail.com>
2019-01-28 17:19:01 +0100

    Created TroopServiceTest

---
+++ src/test/java/com/greenfox/tribes1/Troop/TroopServiceTest.java
@@ -0,0 +1,41 @@
+package com.greenfox.tribes1.Troop;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.junit.Assert.*;
+
+import static org.mockito.Mockito.when;
+
+@RunWith(SpringRunner.class)
+public class TroopServiceTest {
+  private TroopService troopService;
+  private Troop testTroop = new Troop.TroopBuilder().build();
+  private Troop nullTroop = null;
+
+  @Mock
+  private TroopRepository troopRepository;
+
+  @Before
+  public void init() {
+    MockitoAnnotations.initMocks(this);
+    troopService = new TroopService(troopRepository);
+  }
+
+  @Test
+  public void save_successful() {
+    when(troopRepository.save(testTroop)).thenReturn(testTroop);
+    assertEquals(troopService.save(testTroop), testTroop);
+  }
+
+  @Test
+  public void save_unsuccessful() {
+    when(troopRepository.save(nullTroop)).thenReturn(null);
+    assertEquals(troopService.save(nullTroop), nullTroop);
+  }
+}
