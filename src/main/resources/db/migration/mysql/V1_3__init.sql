e83ee557e071e3bf99a3f5d54216bb677785a6d0
ChrisFrill <fkrisztian93@gmail.com>
2019-01-29 12:47:18 +0100

    Refactored V_1_3

--- src/main/resources/db/migration/mysql/V1_3__init.sql
+++ src/main/resources/db/migration/mysql/V1_3__init.sql
@@ -1,18 +1,19 @@
 CREATE TABLE `troop` (
   `id`           bigint(20) NOT NULL AUTO_INCREMENT,
+  `level`        bigint(20) UNSIGNED NOT NULL,
   `HP`           bigint(20) DEFAULT NULL,
   `attack`       bigint(20) DEFAULT NULL,
   `defense`      bigint(20) DEFAULT NULL,
-  `started_at`   bigint(20) DEFAULT NULL,
-  `finished_at`  bigint(20) DEFAULT NULL,
+  `started_at`   TIMESTAMP (6),
+  `finished_at`  TIMESTAMP (6),
   PRIMARY KEY (`id`)
 );
 
-CREATE TABLE `kingdom_troop` (
-  `troop_id`          bigint(20) NOT NULL,
+CREATE TABLE `kingdom_troops` (
+  `troops_id`  bigint(20) NOT NULL,
   `kingdom_id` bigint(20) NOT NULL,
   PRIMARY KEY (`kingdom_id`),
-  KEY troop_id (`troop_id`),
+  KEY troops_id (`troops_id`),
   CONSTRAINT kingdom_troop_kingdom_id FOREIGN KEY (`kingdom_id`) REFERENCES `kingdom` (`id`),
-  CONSTRAINT kingdom_troop_troop_id FOREIGN KEY (`troop_id`) REFERENCES `troop` (`id`)
+  CONSTRAINT kingdom_troop_troops_id FOREIGN KEY (`troops_id`) REFERENCES `troop` (`id`)
 );
