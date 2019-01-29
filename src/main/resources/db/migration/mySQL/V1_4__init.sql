71ea62bb1212636878b79d62990ac254bc14496a
lyancsie <info@fordit.biz>
2019-01-29 11:09:42 +0100

    random FK and UK names are changed in hibernate-generated SQL tables

--- src/main/resources/db/migration/mySQL/V1_4__init.sql
+++ src/main/resources/db/migration/mySQL/V1_4__init.sql
@@ -9,7 +9,7 @@ CREATE TABLE `kingdom_resources` (
   `resources_id` bigint(20) DEFAULT NULL,
   `kingdom_id`   bigint(20) NOT NULL,
   PRIMARY KEY (`kingdom_id`),
-  UNIQUE KEY `UK_fkrih015uhtdh6kqoghpumk60` (`resources_id`),
-  CONSTRAINT `FKgcnhneeg1ms2eojpn71ysw8lo` FOREIGN KEY (`resources_id`) REFERENCES `kingdom` (`id`),
-  CONSTRAINT `FKjjxt7p67b7npt8xeqvo49dmpo` FOREIGN KEY (`kingdom_id`) REFERENCES `kingdom_resource` (`id`)
+  UNIQUE KEY `UK_resources_id` (`resources_id`),
+  CONSTRAINT `FK_kingdom_id` FOREIGN KEY (`resources_id`) REFERENCES `kingdom` (`id`),
+  CONSTRAINT `FK_kingdom_resource_id` FOREIGN KEY (`kingdom_id`) REFERENCES `kingdom_resource` (`id`)
 );
