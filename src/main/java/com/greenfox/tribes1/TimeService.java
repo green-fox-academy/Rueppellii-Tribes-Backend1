2860a1e256147c07abd8b9a184286f26bd670d20
refike <daniel.megyaszai@gmail.com>
2019-01-30 11:35:45 +0100

    small refactorio

--- src/main/java/com/greenfox/tribes1/TimeService.java
+++ src/main/java/com/greenfox/tribes1/TimeService.java
@@ -8,11 +8,11 @@ import java.util.concurrent.TimeUnit;
 @Service
 public class TimeService {
 
-  public Long calculateDifference(Timestamp startedDate, Timestamp finishedDate) throws DateNotGivenException {
-    if (finishedDate == null || startedDate == null) {
+  public Long calculateDifference(Timestamp started_at, Timestamp finished_at) throws DateNotGivenException {
+    if (finished_at == null || started_at == null) {
       throw new DateNotGivenException("Date not given!");
     }
-    long difference = finishedDate.getTime() - startedDate.getTime();
+    long difference = finished_at.getTime() - started_at.getTime();
     return TimeUnit.MILLISECONDS.toMinutes(difference);
   }
 }
