8720c434a7e8ec9e107e140a56de2f5000b54ffe
ChrisFrill <fkrisztian93@gmail.com>
2019-01-30 11:50:16 +0100

    Added Builder

--- src/main/java/com/greenfox/tribes1/ApplicationUser/DTO/ApplicationUserWithKingdomDTO.java
+++ src/main/java/com/greenfox/tribes1/ApplicationUser/DTO/ApplicationUserWithKingdomDTO.java
@@ -1,14 +1,12 @@
 package com.greenfox.tribes1.ApplicationUser.DTO;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
+@Builder
 public class ApplicationUserWithKingdomDTO {
   private Long id;
   private String username;
