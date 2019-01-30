49feac78bda0192d957e07de530a1c69724540eb
ChrisFrill <fkrisztian93@gmail.com>
2019-01-30 11:49:44 +0100

    Added Builder

--- src/main/java/com/greenfox/tribes1/ApplicationUser/DTO/ApplicationUserDTO.java
+++ src/main/java/com/greenfox/tribes1/ApplicationUser/DTO/ApplicationUserDTO.java
@@ -1,14 +1,14 @@
 package com.greenfox.tribes1.ApplicationUser.DTO;
 
-import lombok.Getter;
-import lombok.Setter;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 import javax.validation.constraints.NotBlank;
 
 @Getter
 @Setter
 @NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class ApplicationUserDTO {
   @NotBlank
   private String username;
@@ -16,8 +16,4 @@ public class ApplicationUserDTO {
   private String password;
   private String kingdomName;
 
-  public ApplicationUserDTO(String username, String password) {
-    this.username = username;
-    this.password = password;
-  }
 }
