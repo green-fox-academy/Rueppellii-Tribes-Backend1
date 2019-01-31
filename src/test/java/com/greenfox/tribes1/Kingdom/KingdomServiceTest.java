70070feab2566eb0d17a9ef057bad7b04a0ab230
Peter Kovacs <pitner33@gmail.com>
2019-01-31 10:38:02 +0100

    (add) add tests to the new KindomService methods

--- src/test/java/com/greenfox/tribes1/Kingdom/KingdomServiceTest.java
+++ src/test/java/com/greenfox/tribes1/Kingdom/KingdomServiceTest.java
@@ -1,13 +1,22 @@
 package com.greenfox.tribes1.Kingdom;
 import com.greenfox.tribes1.Exception.NotValidKingdomNameException;
+import com.greenfox.tribes1.Kingdom.DTO.KingdomDTO;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
+import org.modelmapper.ModelMapper;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.web.client.HttpClientErrorException;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 import static org.junit.Assert.*;
+import static org.mockito.Mockito.when;
 
 @RunWith(SpringRunner.class)
 public class KingdomServiceTest {
@@ -17,7 +26,11 @@ public class KingdomServiceTest {
   KingdomRepository kingdomRepository;
 
   private Kingdom validKingdom = new Kingdom("Narnia");
+  private Kingdom validKingdom2 = new Kingdom("Rueppellii");
   private Kingdom notValidKingdom = new Kingdom(null);
+  private KingdomDTO kingdomDTO = new ModelMapper().map(validKingdom, KingdomDTO.class);
+  private List<Kingdom> testList = new ArrayList<>();
+
 
   @Before
   public void init() {
@@ -26,13 +39,33 @@ public class KingdomServiceTest {
   }
 
   @Test
-  public void saveKingdomWithValidNameReturnKingdomWithValidName() throws NotValidKingdomNameException {
+  public void saveKingdom_WithValidName_ReturnKingdomWithValidName() throws NotValidKingdomNameException {
     Mockito.when(kingdomRepository.save(validKingdom)).thenReturn(validKingdom);
     assertEquals(kingdomService.saveKingdom(validKingdom), validKingdom);
   }
 
   @Test(expected = NotValidKingdomNameException.class)
-  public void saveKingdomWithInvalidNameReturnException() throws NotValidKingdomNameException {
+  public void saveKingdom_WithInvalidName_ReturnException() throws NotValidKingdomNameException {
     kingdomService.saveKingdom(notValidKingdom);
+  }
+
+  @Test
+  public void createKingdomDTOFromKingdom_GivesCorrectFieldValues() {
+    assertEquals(kingdomService.createKingdomDTOFromKingdom(validKingdom).getKingdomName(), kingdomDTO.getKingdomName());
+    assertEquals(kingdomService.createKingdomDTOFromKingdom(validKingdom).getId(), kingdomDTO.getId());
+    assertEquals(kingdomService.createKingdomDTOFromKingdom(validKingdom).getApplicationUserName(), kingdomDTO.getApplicationUserName());
+  }
+
+  @Test(expected = IllegalArgumentException.class)
+  public void createKingdomDTOFromKingdom_GivesError_IfNoKingdomProvided() {
+    kingdomService.createKingdomDTOFromKingdom(null);
+  }
+
+  @Test
+  public void findAll_GivesCorrectListOfKingdoms() {
+    testList.add(validKingdom);
+    testList.add(validKingdom2);
+    when(kingdomRepository.findAll()).thenReturn(Arrays.asList(validKingdom, validKingdom2));
+    assertEquals(kingdomService.findAll(), testList);
   }
 }
