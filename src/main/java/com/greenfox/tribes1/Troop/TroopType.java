b0d168050c9bfc1c6d23b1596f29b0e07e47efeb
lyancsie <info@fordit.biz>
2019-02-04 17:43:25 +0100

    two troop types implemented

--- src/main/java/com/greenfox/tribes1/Troop/TroopType.java
+++ src/main/java/com/greenfox/tribes1/Troop/TroopType.java
@@ -1,17 +1,30 @@
 package com.greenfox.tribes1.Troop;
 
+import com.greenfox.tribes1.Troop.Model.Archer;
+import com.greenfox.tribes1.Troop.Model.Swordsman;
 import com.greenfox.tribes1.Troop.Model.TestTroop;
 import com.greenfox.tribes1.Troop.Model.Troop;
 
 public enum TroopType {
-
+  
   TestTroop {
     public Troop makeTroop() {
       return new TestTroop();
     }
+  },
+  
+  archer {
+    public Troop makeTroop() {
+      return new Archer();
+    }
+  },
+  swordsman {
+    public Troop makeTroop() {
+      return new Swordsman();
+    }
   };
-
-  public Troop makeTroop() {
+  
+  public Troop makeTroop(){
     return null;
   }
 }
