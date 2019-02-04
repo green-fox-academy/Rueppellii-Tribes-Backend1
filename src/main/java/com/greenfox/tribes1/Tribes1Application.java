4320cea277b8c340b8aabbe444e944f6139f6f24
lyancsie <info@fordit.biz>
2019-02-05 00:37:24 +0100

    imported guava for using Preconditions

--- src/main/java/com/greenfox/tribes1/Tribes1Application.java
+++ src/main/java/com/greenfox/tribes1/Tribes1Application.java
@@ -5,11 +5,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Tribes1Application {
-
+  
   public static void main(String[] args) {
-
+    
     SpringApplication.run(Tribes1Application.class, args);
   }
-
+  
 }
 
--- src/main/java/com/greenfox/tribes1/Troop/TroopFactory.java
+++ src/main/java/com/greenfox/tribes1/Troop/TroopFactory.java
@@ -1,19 +1,30 @@
 package com.greenfox.tribes1.Troop;
 
+import com.google.common.base.Preconditions;
 import com.greenfox.tribes1.Resources.Food;
 import com.greenfox.tribes1.Troop.Model.Troop;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class TroopFactory {
   
   public Troop makeTroop(TroopType troopType) {
     
-    troopType.makeTroop()
+    List<Food> feed = troopType.makeTroop()
         .getKingdom()
         .getResources()
         .stream()
         .filter(resource -> resource instanceof Food)
-        .filter(food -> food.getAmount() > 0)
-        .forEach(x -> x.setAmountPerMinute(x.getAmountPerMinute() - 1));
+        .map(f -> (Food) f)
+        .collect(Collectors.toList());
+    
+    Preconditions.checkArgument(feed.size() == 1);
+    
+    Food food = feed.get(0);
+    food.setAmountPerMinute(food.getAmountPerMinute() - 1);
+    
+    // TODO: save food
     
     return troopType.makeTroop();
   }
