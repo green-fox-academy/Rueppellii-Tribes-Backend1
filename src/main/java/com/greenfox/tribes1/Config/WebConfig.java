90fa001c249f8bcffd15004d5560c62813061caf
ChrisFrill <fkrisztian93@gmail.com>
2019-02-04 15:34:40 +0100

    Refactored

--- src/main/java/com/greenfox/tribes1/Config/WebConfig.java
+++ src/main/java/com/greenfox/tribes1/Config/WebConfig.java
@@ -1,13 +1,9 @@
 package com.greenfox.tribes1.Config;
 
 import com.greenfox.tribes1.Logging.LoggerInterceptor;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import org.springframework.web.servlet.handler.SimpleMappingExceptionResolver;
-
-import java.util.Properties;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
--- src/main/java/com/greenfox/tribes1/Logging/LoggerInterceptor.java
+++ src/main/java/com/greenfox/tribes1/Logging/LoggerInterceptor.java
@@ -2,9 +2,7 @@ package com.greenfox.tribes1.Logging;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
-
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
@@ -14,16 +12,14 @@ public class LoggerInterceptor extends HandlerInterceptorAdapter {
 
   @Override
   public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
- /*   log.info("[preHandle][" + request + "]" + "[" + request.getMethod()
-            + "]" + request.getRequestURI());*/
-    log.info("Request URI       : {}", request.getRequestURI());
-    log.info("Request method    : {}", request.getMethod());
-    log.info("Request body      : {}", request.getInputStream());
-    return true;
+    log.info("Request URI     : {}", request.getRequestURI());
+    log.info("Request method  : {}", request.getMethod());
+    return super.preHandle(request, response, handler);
   }
 
   @Override
-  public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
-    log.info("Response status   : {}", response.getStatus());
+  public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
+    log.info("Response status : {}", response.getStatus());
+    super.afterCompletion(request, response, handler, ex);
   }
 }
