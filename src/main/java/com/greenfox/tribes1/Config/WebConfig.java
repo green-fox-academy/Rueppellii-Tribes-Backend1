d9e12b7cbfc758c3c1f53766c7f1e5c45277aeec
ChrisFrill <fkrisztian93@gmail.com>
2019-02-04 11:13:55 +0100

    Added WebConfig

---
+++ src/main/java/com/greenfox/tribes1/Config/WebConfig.java
@@ -0,0 +1,20 @@
+package com.greenfox.tribes1.Config;
+
+import com.greenfox.tribes1.Logging.LoggerInterceptor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.handler.SimpleMappingExceptionResolver;
+
+import java.util.Properties;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+  @Override
+  public void addInterceptors(InterceptorRegistry registry) {
+    registry.addInterceptor(new LoggerInterceptor());
+  }
+
+}
