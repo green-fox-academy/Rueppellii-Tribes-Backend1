9b0f58485d91619d8f1ff04028c41ec51c91568d
lyancsie <info@fordit.biz>
2019-02-04 18:26:23 +0100

    minor changes in TroopFactory

--- src/main/java/com/greenfox/tribes1/Troop/TroopFactory.java
+++ src/main/java/com/greenfox/tribes1/Troop/TroopFactory.java
@@ -7,13 +7,13 @@ public class TroopFactory {
   
   public Troop makeTroop(TroopType troopType) {
     
-    Food foodToDecrease = (Food) troopType.makeTroop()
+    troopType.makeTroop()
         .getKingdom()
         .getResources()
         .stream()
         .filter(resource -> resource instanceof Food)
-        .filter(food -> food.getAmount() > 0);
-    foodToDecrease.setAmountPerMinute(foodToDecrease.getAmountPerMinute() - 1);
+        .filter(food -> food.getAmount() > 0)
+        .forEach(x -> x.setAmountPerMinute(x.getAmountPerMinute() - 1));
     
     return troopType.makeTroop();
   }
