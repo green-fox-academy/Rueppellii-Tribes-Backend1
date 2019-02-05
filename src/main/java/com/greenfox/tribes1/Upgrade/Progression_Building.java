2364d4dd1b52c36554ba0fd2fd695cd0e0a44579
Enikő Tóthová <toeraab.avf@gmail.com>
2019-02-05 10:47:23 +0100

    progression model in progress

---
+++ src/main/java/com/greenfox/tribes1/Upgrade/Progression_Building.java
@@ -0,0 +1,5 @@
+package com.greenfox.tribes1.Upgrade;
+
+
+public class Progression_Building extends Progression {
+}
---
+++ src/main/java/com/greenfox/tribes1/Upgrade/Progression_Troop.java
@@ -0,0 +1,5 @@
+package com.greenfox.tribes1.Upgrade;
+
+public class Progression_Troop extends Progression{
+
+}
