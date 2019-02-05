a137fdb033f7ad04487648e683dc266880b1e06b
lyancsie <info@fordit.biz>
2019-02-05 01:31:24 +0100

    modified TroopServiceTest because TroopFactory constructor had been changed

--- src/main/java/com/greenfox/tribes1/Troop/TroopFactory.java
+++ src/main/java/com/greenfox/tribes1/Troop/TroopFactory.java
@@ -1,15 +1,12 @@
 package com.greenfox.tribes1.Troop;
 
-import com.google.common.base.Preconditions;
 import com.google.common.collect.Iterables;
 import com.greenfox.tribes1.Exception.NotValidResourceException;
 import com.greenfox.tribes1.Resources.Food;
 import com.greenfox.tribes1.Resources.ResourceService;
 import com.greenfox.tribes1.Troop.Model.Troop;
-import lombok.SneakyThrows;
 import org.springframework.beans.factory.annotation.Autowired;
 
-import java.util.List;
 import java.util.stream.Collectors;
 
 public class TroopFactory {
@@ -25,22 +22,20 @@ public class TroopFactory {
     
     //List<Food> feed =
     Food food =
-    Iterables.getOnlyElement(
-        troopType.makeTroop()
-            .getKingdom()
-            .getResources()
-            .stream()
-            .filter(resource -> resource instanceof Food)
-            .map(f -> (Food) f)
-            .collect(Collectors.toList()));
+        Iterables.getOnlyElement(
+            troopType.makeTroop()
+                .getKingdom()
+                .getResources()
+                .stream()
+                .filter(resource -> resource instanceof Food)
+                .map(f -> (Food) f)
+                .collect(Collectors.toList()));
     
-    //Preconditions.checkArgument(feed.size() == 1);
+    //Preconditions.checkArgument(feed.size() == 1); Not needed due to getOnlyElement
     
     food.setAmountPerMinute(food.getAmountPerMinute() - 1);
     resourceService.saveResource(food);
     
-    // TODO: save food
-    //TODO: Iterables.getOnlyElement
     return troopType.makeTroop();
   }
 }
