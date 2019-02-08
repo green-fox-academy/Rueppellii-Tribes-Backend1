4eb94b7a618d58004d39f45a36bc9eab49309104
ChrisFrill <fkrisztian93@gmail.com>
2019-02-08 16:25:48 +0100

    Added resources to test database

--- src/test/resources/afterTestRun.sql
+++ src/test/resources/afterTestRun.sql
@@ -1,7 +1,26 @@
-DELETE FROM application_user_kingdom;
-DELETE FROM kingdom_buildings;
-DELETE FROM kingdom_troops;
-DELETE FROM application_user;
-DELETE FROM kingdom;
-DELETE FROM troop;
-DELETE FROM building;
+DELETE
+FROM application_user_kingdom;
+
+DELETE
+FROM kingdom_buildings;
+
+DELETE
+FROM kingdom_troops;
+
+DELETE
+FROM kingdom_resources;
+
+DELETE
+FROM application_user;
+
+DELETE
+FROM kingdom;
+
+DELETE
+FROM troop;
+
+DELETE
+FROM building;
+
+DELETE
+FROM kingdom_resource;
--- src/test/resources/beforeTestRun.sql
+++ src/test/resources/beforeTestRun.sql
@@ -1,15 +1,37 @@
-DELETE FROM application_user_kingdom;
-DELETE FROM kingdom_buildings;
-DELETE FROM kingdom_troops;
-DELETE FROM application_user;
-DELETE FROM kingdom;
-DELETE FROM troop;
-DELETE FROM building;
+DELETE
+FROM application_user_kingdom;
 
+DELETE
+FROM kingdom_buildings;
+
+DELETE
+FROM kingdom_troops;
+
+DELETE
+FROM kingdom_resources;
+
+DELETE
+FROM application_user;
+
+DELETE
+FROM kingdom;
+
+DELETE
+FROM troop;
+
+DELETE
+FROM building;
+
+DELETE
+FROM kingdom_resource;
+
+-- testUser1 password: pass1
+-- testUser2 password: pass2
+-- testUser3 password: pass3
 INSERT INTO application_user(id, password, user_email, username)
-VALUES (1, 'pass1', 'test1@test.test', 'testUser1'),
-       (2, 'pass2', 'test2@test.test', 'testUser2'),
-       (3, 'pass3', 'test3@test.test', 'testUser3');
+VALUES (1, '$2a$10$3A7YK9hDUpHN4plBoCphYOzk426CebJwnaFMk0kN4qEXoWUTiwejC', 'test1@test.test', 'testUser1'),
+       (2, '$2a$10$y1WkKt52SH8eDm6zvy63v.B0EstAaevqAgfo7plk8v9UuigsMcqxi', 'test2@test.test', 'testUser2'),
+       (3, '$2a$10$N.4V.83hs.5X2bI0qY0Tme2PYceHtDf2Suzh0QHEcYVZxeS0YhJL6', 'test3@test.test', 'testUser3');
 
 INSERT INTO kingdom(id, name)
 VALUES (1, 'testKingdom1'),
@@ -37,6 +59,16 @@ VALUES (1, 1),
        (3, 3);
 
 INSERT INTO kingdom_troops(troops_id, kingdom_id)
+VALUES (1, 1),
+       (2, 2),
+       (3, 3);
+
+INSERT INTO kingdom_resource(id, amount, updated_at)
+VALUES (1, 0, '2019-02-04 08:00:00'),
+       (2, 10, '2019-02-10 16:00:00'),
+       (3, 500, '2019-02-11 10:00:00');
+
+INSERT INTO kingdom_resources(resources_id, kingdom_id)
 VALUES (1, 1),
        (2, 2),
        (3, 3);
