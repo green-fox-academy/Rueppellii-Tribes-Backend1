a20d76d1f2e76f74de954a9571373bae81aef0bc
Peter Kovacs <pitner33@gmail.com>
2019-02-12 15:58:25 +0100

    (fix) webconfig enpoint exclusions added

--- src/main/java/com/greenfox/tribes1/Config/WebConfig.java
+++ src/main/java/com/greenfox/tribes1/Config/WebConfig.java
@@ -8,12 +8,19 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
+
+  List<String> excludeEndpoints = Arrays.asList("/register", "/login");
 
   @Override
   public void addInterceptors(InterceptorRegistry registry) {
     registry.addInterceptor(new LoggerInterceptor());
-    registry.addInterceptor(new ProgressionInterceptor());
+    registry.addInterceptor(new ProgressionInterceptor()).excludePathPatterns(excludeEndpoints);
   }
 }
