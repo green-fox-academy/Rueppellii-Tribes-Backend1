347616277ac6a45c1ac782e275e51cece308fab4
lyancsie <info@fordit.biz>
2019-02-14 23:35:24 +0100

    unnecessary @Transient building removed from Resource, but connection need to be made somehow

--- src/main/java/com/greenfox/tribes1/Resources/Resource.java
+++ src/main/java/com/greenfox/tribes1/Resources/Resource.java
@@ -1,7 +1,6 @@
 package com.greenfox.tribes1.Resources;
 
 import com.fasterxml.jackson.annotation.JsonBackReference;
-import com.greenfox.tribes1.Building.Building;
 import com.greenfox.tribes1.Kingdom.Kingdom;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -25,10 +24,6 @@ public abstract class Resource implements Updatable {
   private Long amount;
   private Timestamp updated_at;
   private Long amountPerMinute;
-
-  //Todo: decide if we need this + if we need @Transient here
-  @Transient
-  private Building building;
 
   @ManyToOne(fetch = FetchType.EAGER)
   @JoinColumn(name = "kingdom_id", referencedColumnName = "id")
