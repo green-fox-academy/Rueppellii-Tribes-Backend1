34cea998cded99043841be9e296d3b79f59e9a2b
lyancsie <info@fordit.biz>
2019-02-14 23:38:43 +0100

    @DiscriminatorValue added to Gold

--- src/main/java/com/greenfox/tribes1/Resources/Gold.java
+++ src/main/java/com/greenfox/tribes1/Resources/Gold.java
@@ -3,9 +3,11 @@ package com.greenfox.tribes1.Resources;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.persistence.DiscriminatorValue;
 import javax.persistence.Entity;
 
 @Entity(name = "Gold")
+@DiscriminatorValue("Gold")
 @Getter
 @Setter
 public class Gold extends Resource {
