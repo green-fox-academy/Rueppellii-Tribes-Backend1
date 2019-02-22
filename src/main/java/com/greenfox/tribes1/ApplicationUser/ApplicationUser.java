8f13e80852f7d0ed3bcae64620d835327c8e4cf5
ChrisFrill <fkrisztian93@gmail.com>
2019-02-22 21:03:30 +0100

    Updated ApplicationUser with Role connection

--- src/main/java/com/greenfox/tribes1/ApplicationUser/ApplicationUser.java
+++ src/main/java/com/greenfox/tribes1/ApplicationUser/ApplicationUser.java
@@ -2,9 +2,11 @@ package com.greenfox.tribes1.ApplicationUser;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.greenfox.tribes1.Kingdom.Kingdom;
+import com.greenfox.tribes1.Role.Role;
 import lombok.*;
 
 import javax.persistence.*;
+import java.util.Set;
 
 @Entity
 @Builder
@@ -22,9 +24,18 @@ public class ApplicationUser {
   private String password;
   private String userEmail;
   
-  @OneToOne(cascade = CascadeType.PERSIST)
+  @OneToOne(cascade = {CascadeType.PERSIST, CascadeType.REMOVE})
   @JoinColumn(name = "kingdom_id")
   private Kingdom kingdom;
+
+  @ManyToMany(cascade = {CascadeType.PERSIST, CascadeType.DETACH}, fetch = FetchType.EAGER)
+  @JoinTable(
+          name = "application_users_roles",
+          joinColumns = @JoinColumn(
+                  name = "application_user_id", referencedColumnName = "id"),
+          inverseJoinColumns = @JoinColumn(
+                  name = "role_id", referencedColumnName = "id"))
+  private Set<Role> roles;
   
   public ApplicationUser(String username, String password, String userEmail) {
     this.username = username;
